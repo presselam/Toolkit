f415c64917650da582e9bd3e3b47b55f5318c7b2
DESKTOP-RMOEUP1\Andrew Pressel <apressel@nextgenfed.com>
2017-09-08 08:36:01 -0400

    baseline java utilities.

---
+++ java/src/com/pressel/toolkit/CountSummary.java
@@ -0,0 +1,16 @@
+package com.pressel.toolkit;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Andrew Pressel
+ */
+public class CountSummary extends HashMap<Object, Integer> {
+
+  public CountSummary() {
+
+  }
+}
